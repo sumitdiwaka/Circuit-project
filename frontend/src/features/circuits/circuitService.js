172babd039ee25fbdca77603ae43c84371bc8786
unknown <sumitdiwakar476@gmai.com>
2025-10-03 20:39:12 +0530

    url

--- frontend/src/features/circuits/circuitService.js
+++ frontend/src/features/circuits/circuitService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/circuits/';
+const API_URL = '/api/circuits/'|| 'https://circuit-project.onrender.com/';
 
 const getAuthHeaders = () => {
   const user = JSON.parse(localStorage.getItem('user'));
